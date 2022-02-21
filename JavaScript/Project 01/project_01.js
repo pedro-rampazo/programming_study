e066d0c9ea274d3603cffbe56a31473ce4888050
Pedro Rampazo <pedro.rampazo@hotmail.com>
2022-02-21 20:55:06 -0300

    commit 21/02/2022 20:55

--- JavaScript/Project 01/project_01.js
+++ JavaScript/Project 01/project_01.js
@@ -2,5 +2,6 @@ const submit = document.querySelectorAll('.submit')[0];
 
 submit.addEventListener('click', ()=>{
     let numbers = document.getElementsByClassName('sum_values');
-    console.log(numbers);
+    let result = parseInt(numbers[0].value) + parseInt(numbers[1].value);
+    alert(`Result: ${numbers[0].value} + ${numbers[1].value} = ${result}`);
 })
