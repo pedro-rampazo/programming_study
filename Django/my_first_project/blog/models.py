167888bf7d39c61edf3dbed57fef5255cf378b3a
Pedro Rampazo <pedro.rampazo@hotmail.com>
2022-03-17 17:59:47 -0300

    commit 17/03/2022 17:59

--- Django/my_first_project/blog/models.py
+++ Django/my_first_project/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -10,6 +11,14 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ("-created",)
+
 
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug})
+    
